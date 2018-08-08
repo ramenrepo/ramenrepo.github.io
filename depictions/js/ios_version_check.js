f435b89a9ecff34f7788be0097654772eea4dbd8
= <=>
2018-08-08 03:34:35 -0400

    General update

--- depictions/js/ios_version_check.js
+++ depictions/js/ios_version_check.js
@@ -46,10 +46,10 @@ Twitter - @TimonOlsthoorn
 
 
 // changed const to var for IE9/10 compatibity.
-var VERSION_CHECK_SUPPORTED = "Your iOS version is supported! &#x1f60a;";
-var VERSION_CHECK_NEEDS_UPGRADE = "Requires at least iOS %s &#x1f615;";
-var VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s &#x1f601;";
-var VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f61e;";
+var VERSION_CHECK_SUPPORTED = "Your iOS version is supported! &#x1f638;";
+var VERSION_CHECK_NEEDS_UPGRADE = "Requires at least iOS %s &#x1f640;";
+var VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s &#x1f640;";
+var VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f63f;";
 
 function ios_version_check(minIOS,maxIOS,otherIOS,callBack) {
 	"use strict";
